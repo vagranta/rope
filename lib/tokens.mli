b21f3fa9a61b6a714eadc872665697aa81368eb2
Leo <breidingleo2@gmail>
2025-11-28 16:52:12 -0500

    New changes to tokens and main

--- lib/tokens.mli
+++ lib/tokens.mli
@@ -24,3 +24,28 @@ type token =
   | DIV
   | MOD
   | EOF
+
+let show_token = function
+  | LET -> "LET"
+  | FUN -> "FUN"
+  | IF -> "IF"
+  | THEN -> "THEN"
+  | ELSE -> "ELSE"
+  | TYPE -> "TYPE"
+  | INCLUDE -> "INCLUDE"
+  | INT i -> "INT(" ^ string_of_int i ^ ")"
+  | BOOL b -> "BOOL(" ^ string_of_bool b ^ ")"
+  | STRING s -> "STRING(" ^ s ^ ")"
+  | IDENT s -> "IDENT(" ^ s ^ ")"
+  | COLON -> "COLON"
+  | ARROW -> "ARROW"
+  | EQ -> "EQ"
+  | LPAREN -> "LPAREN"
+  | RPAREN -> "RPAREN"
+  | COMMA -> "COMMA"
+  | PLUS -> "PLUS"
+  | MINUS -> "MINUS"
+  | MULT -> "MULT"
+  | DIV -> "DIV"
+  | MOD -> "MOD"
+  | EOF -> "EOF"
